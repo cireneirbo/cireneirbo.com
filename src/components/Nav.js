165d3890a7d78cdbe4ba65aa8d64786382dfa242
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2021-11-10 10:09:27 -0500

    Added components

---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import './Nav.css';
+
+function Nav() {
+  return (
+    <div className="Nav">
+        <nav id="nav">
+            <ul className="container">
+                <li><a href="#top">Top</a></li>
+                <li><a href="#work">Work</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+  );
+}
+
+export default Nav;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Header from './components/Header';
+import Hello from './components/Hello';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+     <Header />
+     <Nav />
+     <Hello />
+     
+     <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
