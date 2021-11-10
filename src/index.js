ba54b672101da65fce5bb29cea54bf2311844725
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2021-11-10 10:38:09 -0500

    Add several components

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Header from './components/Header';
-import Hello from './components/Hello';
 import Nav from './components/Nav';
+import Hello from './components/Hello';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 ReactDOM.render(
@@ -12,7 +14,8 @@ ReactDOM.render(
      <Header />
      <Nav />
      <Hello />
-     
+     <Portfolio />
+     <Contact />
      <Footer />
   </React.StrictMode>,
   document.getElementById('root')
