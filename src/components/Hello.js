5f38f7d20038b2ca8f3414e417e8ac6f285fd8c4
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2021-11-10 13:27:25 -0500

    Clean up component code

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -12,7 +12,7 @@ function Hello() {
             </div>
             <div className="col-8 col-7-large col-12-medium">
               <header>
-                <h1>Hi. I'm <strong>Jane Doe</strong>.</h1>
+                <h1>Hi. I'm <strong>Eric O'Brien</strong>.</h1>
               </header>
               <p>And this is <strong>Miniport</strong>, a free, fully responsive HTML5 site template designed by <a href="http://twitter.com/ajlkn">AJ</a> for <a href="http://html5up.net">HTML5 UP</a> &amp; released under the <a href="http://html5up.net/license">CCA license</a>.</p>
               <a href="#work" className="button large scrolly">Learn about what I do</a>
