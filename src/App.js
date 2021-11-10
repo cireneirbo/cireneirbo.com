e14d2f757872a99e831bd2e486a80431522d47c6
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2021-11-10 11:43:12 -0500

    Clean up index.js and add all components in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import './App.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Hello from './components/Hello';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
+  return (
+    <div>
+      <Header />
+      <Nav />
+      <Hello />
+      <About />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,21 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Header from './components/Header';
-import Nav from './components/Nav';
-import Hello from './components/Hello';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
 
 ReactDOM.render(
   <React.StrictMode>
-     <Header />
-     <Nav />
-     <Hello />
-     <Portfolio />
-     <Contact />
-     <Footer />
+     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
