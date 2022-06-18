56cba5f831fce289315c931224ce8377511adf70
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2022-06-18 19:04:16 -0400

    Move Articles in Portfolio to an array and use a map to disply

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,14 +1,77 @@
+/* Components*/
+import Header from '../components/header/Header';
+import Footer from '../components/footer/Footer';
+import Article from '../components/article/Article';
+/* Assets */
 import picPasswordMaker from '../assets/password-maker.png';
 import picTMNT from '../assets/tmnt-code.png';
 import picDiscordBot from '../assets/discord-bot-of-hacktoberfest-2021.png';
 import picAirwinfi from '../assets/airwinfi-code.png';
 import picCireNeirboCom from '../assets/cireneirbocom-code.png';
 import picEos from '../assets/eos-code.png';
-import Header from '../components/header/Header';
-import Footer from '../components/footer/Footer';
-import Article from '../components/article/Article';
+import picUncommonGroundsCafe from '../assets/uncommon-grounds-cafe.png';
 
 function Portfolio() {
+
+	const projects = [
+		{
+			title: "Earth Observation System",
+			aHrefRepo: "https://github.com/cireneirbo/Earth-Observation-System",
+			aHrefDemo: "https://cireneirbo.github.io/password-maker/",
+			aImage: picEos,
+			description: "A system for alerting users to local and regional disasters using the NASA EONET API.",
+			technologies: "Node.js, React.js, Express.js, REST API"
+		},
+		{
+			title: "Turtles In A Whole Shell",
+			aHrefRepo: "https://github.com/cireneirbo/turtles-in-a-whole-shell",
+			aHrefDemo: "https://cireneirbo.github.io/password-maker/",
+			aImage: picTMNT,
+			description: "An Express/React app that shows information about the Teenage Mutant Ninja Turtles on various web pages.",
+			technologies: "Node.js, Express.js"
+		},
+		{
+			title: "discord-bot-of-hacktoberfest-2021",
+			aHrefRepo: "https://github.com/cireneirbo/discord-bot-of-hacktoberfest-2021",
+			aHrefDemo: "https://cireneirbo.github.io/password-maker/",
+			aImage: picDiscordBot,
+			description: "A community-driven project for the 2021 Hacktoberfest hackathon.",
+			technologies: "Node.js, Discord.js"
+		},
+		{
+			title: "Password-Maker",
+			aHrefRepo: "https://github.com/cireneirbo/password-maker",
+			aHrefDemo: "https://cireneirbo.github.io/password-maker/",
+			aImage: picPasswordMaker,
+			description: "A website that creates a strong password for the user based on several user inputs.",
+			technologies: "ES6+"
+		},
+		{
+			title: "Airwinfi",
+			aHrefRepo: "https://github.com/cireneirbo/airwinfi",
+			aHrefDemo: "https://cireneirbo.github.io/password-maker/",
+			aImage: picAirwinfi,
+			description: "A site that shows you where you can get free internet or internet paired with the cheapest menu item at the business.",
+			technologies: "Node.js, Express.js, REST API"
+		},
+		{
+			title: "Uncommon Grounds Cafe",
+			aHrefRepo: "https://github.com/cireneirbo/uncommon-grounds-cafe",
+			aHrefDemo: "https://cireneirbo.github.io/uncommon-grounds-cafe/",
+			aImage: picUncommonGroundsCafe,
+			description: "A local coffee shop's mobile-friendly website.",
+			technologies: "Node.js, React.js, React-router"
+		},
+		{
+			title: "cireneirbo.com",
+			aHrefRepo: "https://github.com/cireneirbo/cireneirbo.com",
+			aHrefDemo: "https://www.cireneirbo.com/",
+			aImage: picCireNeirboCom,
+			description: "My personal portfolio website, which you happen to be viewing!",
+			technologies: "Node.js, React.js"
+		},
+	];
+
   return (
     <article id="portfolio" className="wrapper style3">
 			<div className="container">
@@ -16,63 +79,22 @@ function Portfolio() {
 				<Header />
 
 				<div className="row">
-					<Article 
-						aHref="https://github.com/cireneirbo/Earth-Observation-System"
-						aHrefDemo="https://cireneirbo.github.io/password-maker/"
-						aImage={picEos}
-						aAlt="Earth Observation System code example."
-						title="Earth Observation System"
-						description="A system for alerting users to local and regional disasters using the NASA EONET API."
-						technologies="Node.js, React.js, Express.js, REST API"
-					/>
-					<Article 
-						aHref="https://github.com/cireneirbo/turtles-in-a-whole-shell"
-						aHrefDemo="https://cireneirbo.github.io/password-maker/"
-						aImage={picTMNT}
-						aAlt="Turtles In A Whole Shell code example."
-						title="Turtles In A Whole Shell"
-						description="An Express/React app that shows information about the Teenage Mutant Ninja Turtles on various web pages."
-						technologies="Node.js, Express.js"
-					/>
-					<Article 
-						aHrefRepo="https://github.com/cireneirbo/discord-bot-of-hacktoberfest-2021"
-						aHrefDemo="https://cireneirbo.github.io/password-maker/"
-						aImage={picDiscordBot}
-						aAlt="discord-bot-of-hacktoberfest-2021 project example."
-						title="discord-bot-of-hacktoberfest-2021"
-						description="A community-driven project for the 2021 Hacktoberfest hackathon."
-						technologies="Node.js, Discord.js"
-					/>
-					<Article 
-						aHrefRepo="https://github.com/cireneirbo/password-maker"
-						aHrefDemo="https://cireneirbo.github.io/password-maker/"
-						aImage={picPasswordMaker}
-						aAlt="Password-Maker code example."
-						title="Password-Maker"
-						description="A website that creates a strong password for the user based on several user inputs."
-						technologies="ES6+"
-					/>
-					<Article 
-						aHref="https://github.com/cireneirbo/airwinfi"
-						aHrefDemo="https://cireneirbo.github.io/password-maker/"
-						aImage={picAirwinfi}
-						aAlt="Airwinfi code example."
-						title="Airwinfi"
-						description="A site that shows you where you can get free internet or internet paired with the cheapest menu item at the business."
-						technologies="Node.js, Express.js, REST API"
-					/>
-					<Article 
-						aHref="https://github.com/cireneirbo/cireneirbo.com"
-						aHrefDemo="https://cireneirbo.github.io/password-maker/"
-						aImage={picCireNeirboCom}
-						aAlt="cireneirbo.com code example."
-						title="cireneirbo.com"
-						description="My personal portfolio website, which you happen to be viewing!"
-						technologies="Node.js, React.js"
-					/>
+
+					{projects.map(project => 
+						<Article 
+						aHrefRepo={project.aHrefRepo}
+						aHrefDemo={project.aHrefDemo}
+						aImage={project.aImage}
+						aAlt={project.title + " demo."}
+						title={project.title}
+						description={project.description}
+						technologies={project.technologies}
+						/>
+					)}
+
 				</div>
 
-				<Footer/>
+				<Footer />
 
 			</div>
     </article>
